9044eea798acfb61d72c9517c35e6879b7474377
Shoupu Wan <wanshoupu@hotmail.com>
2014-12-08 17:23:03 -0800

    Extract form data (files, textarea data, etc.) in python functions

--- forcaster/app.py
+++ forcaster/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request, flash
 from forms import SubmitForm
- 
+import logging
+from logging.handlers import RotatingFileHandler
+
+UPLOAD_FOLDER = '~/tmp'
+ALLOWED_EXTENSIONS = set(['txt', 'csv', 'json'])
+
 app = Flask(__name__) 
- 
 app.secret_key = 'development key'  
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
 @app.route('/')
 def home():
@@ -22,9 +27,12 @@ def submit():
       flash('All fields are required.')
       return render_template('submit.html', form=form)
     else:
-      print form.dataFile.data
-      print form.trainingMode.data
-      print form.trainingData.data
+      trainingdata = ''
+      if form.dataFile.data :
+        trainingdata += request.files[form.dataFile.name].read()
+      if form.trainingData.data :
+        trainingdata += form.trainingData.data
+#      app.logger.info(trainingdata)
  
       return render_template('submit.html', success=True)
  
@@ -32,4 +40,7 @@ def submit():
     return render_template('submit.html', form=form)
 
 if __name__ == '__main__':
+  handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+  handler.setLevel(logging.INFO)
+  app.logger.addHandler(handler)
   app.run(debug=True)
