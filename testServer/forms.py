651ad206b272740e968ee8c5eb99199582953b81
Shoupu Wan <wanshoupu@hotmail.com>
2014-12-10 15:50:40 -0800

    Remove unnecessary subdirectories, lib, and venv (virtual env)

--- testServer/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms.fields import FileField, TextField, TextAreaField, SubmitField, BooleanField
- 
-class ContactForm(Form):
-  name = FileField("Name")
-  email = BooleanField("Email")
-  message = TextAreaField("Message")
-  submit = SubmitField("Send")
